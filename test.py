c33b78aaf3a5f6047f65c606db17245233a80fef
Berk Can Polat <42116059+brkcnplt@users.noreply.github.com>
2025-09-05 22:25:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,6 @@ from datetime import date
 import os
 
 # --- Telegram ayarları ---
-#TOKEN = "8153163023:AAF6TyciGLkjCmr8oXq1hQEO50ahMsGpRmA"
-#CHAT_ID = "1642459289"
-
-# --- Telegram ayarları ---
-# GitHub Actions'ta secrets kullan
 TOKEN = os.environ.get("TELEGRAM_TOKEN", "8153163023:AAF6TyciGLkjCmr8oXq1hQEO50ahMsGpRmA")
 CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "1642459289")
 
@@ -101,8 +96,8 @@ if response.status_code == 200:
         set_last_count(new_count)
     else:
         # hiç değişiklik yok
- message = f"📢 değişiklik yok"
-            send_telegram(message)
+        message = f"📢 değişiklik yok, bildirim sayısı: {new_count}"
+        send_telegram(message)
         print("Yeni bildirim yok, telegrama mesaj gönderilmedi.")
 else:
     send_telegram(f"KAP verisi alınamadı! Status Code: {response.status_code}")
