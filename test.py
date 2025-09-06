d7f74d4e0954156f387b34a7555ee25e478310ee
Berk Can Polat <42116059+brkcnplt@users.noreply.github.com>
2025-09-06 15:21:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "1642459289")
 def send_telegram(message):
     """Telegram mesajı gönder"""
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": message})
+    requests.post(url, data={"chat_id": CHAT_ID, "text": message, "parse_mode": "HTML" })
 
 # --- Son kayıt sayısını dosyada tut ---
 def get_last_count():
